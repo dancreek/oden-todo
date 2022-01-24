532659410798a12a8702dfc6d4c0f6928cc94eb6
dancreek <dancreek@gmail.com>
2022-01-24 15:28:16 -0600

    webpack setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+"use-strict"
+
+import './style.css';
+
+const testLine = document.createElement('div')
+testLine.innerHTML = 'Operational';
+document.body.appendChild(testLine);
+console.log('And away we go!...');
